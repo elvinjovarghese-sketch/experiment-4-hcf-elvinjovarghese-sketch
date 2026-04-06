24a2bbde8b6131207a4a71c24c09b6b3da31b41d
smelvin <elvinjovarghese@gmail.com>
2026-04-06 23:31:12 +0530

    HCF Done

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class HCF {
-    
-        // TODO: Read two integers and print their HCF
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // Find HCF using Euclidean Algorithm
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        // Output
+        System.out.println(a);
+
+        sc.close();
+    }
 }
